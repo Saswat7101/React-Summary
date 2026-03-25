840eb8123d79478d89d37375125310fecf98c3d4
Saswat <mohantysaswat154@gmail.com>
2026-03-25 10:23:01 +0530

    passing data to components with props

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ function App() {
   return (
     <main>
       <Post
-        title="Lorem ipsum dolor sit amet"
-        content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue."
+        title="React.js is Awesome"
+        content="React is a JavaScript library for building user interfaces. It allows developers to create reusable UI components and manage the state of their applications efficiently. With React, you can build complex and interactive web applications with ease."
       />
       <Post
-        title="Lorem ipsum dolor sit amet"
+        title=""
         content="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas congue ligula ac quam viverra nec consectetur ante hendrerit. Donec et mollis dolor. Praesent et diam eget libero egestas mattis sit amet vitae augue."
       />
       <Post
