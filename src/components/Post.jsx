5137a1c60cb33d4528104ec81781f7e378ba1b55
Saswat <mohantysaswat154@gmail.com>
2026-03-25 10:08:01 +0530

    building first component

---
+++ src/components/Post.jsx
@@ -0,0 +1,9 @@
+export default function Post() {
+  return (
+    <div>
+      <h1>My First Post</h1>
+      <p>Saswat</p>
+      <p>This is my first post. I hope you like it!</p>
+    </div>
+  );
+}
