a5a4b71bf8c80fc52219ff2737ced8fdb6f08e6f
Saswat <mohantysaswat154@gmail.com>
2026-03-25 10:12:49 +0530

    outputting dynamic values

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,12 @@
+const names = ["Maximillian", "Manuel"];
+
 export default function Post() {
+  const choosenName = Math.random() > 0.5 ? names[0] : names[1];
+
   return (
     <div>
       <h1>My First Post</h1>
-      <p>Saswat</p>
+      <p>{choosenName}</p>
       <p>This is my first post. I hope you like it!</p>
     </div>
   );
