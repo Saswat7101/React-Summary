0a60cc40a4c3b23a22ea3583ea907143e4ac48c2
Saswat <mohantysaswat154@gmail.com>
2026-03-27 09:59:57 +0530

    preparing the app for state management

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,17 +1,21 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 
 export default function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post
-        author="Maximillian"
-        content="React is awesome because it lets you build dynamic user interfaces efficiently using reusable components and simple state management."
-      />
-      <Post
-        author="Manuel"
-        content="React is awesome because it breaks the UI into reusable components which makes code cleaner, scalable, and easy to maintain."
-      />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post
+          author="Maximillian"
+          content="React is awesome because it lets you build dynamic user interfaces efficiently using reusable components and simple state management."
+        />
+        <Post
+          author="Manuel"
+          content="React is awesome because it breaks the UI into reusable components which makes code cleaner, scalable, and easy to maintain."
+        />
+      </ul>
+    </>
   );
 }
