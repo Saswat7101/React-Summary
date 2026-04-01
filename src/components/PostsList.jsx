8ba1aa7e20a51674a2c32f4b07c1f21095d05fe6
Saswat <mohantysaswat154@gmail.com>
2026-04-01 21:23:59 +0530

    Handle Loading State

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -6,12 +6,17 @@ import Modal from "./Modal";
 
 export default function PostsList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
     async function fetchPosts() {
+      setIsFetching(true);
+
       const response = await fetch("http://localhost:8080/posts");
       const resData = await response.json();
       setPosts(resData.posts);
+
+      setIsFetching(false);
     }
 
     fetchPosts();
@@ -35,19 +40,25 @@ export default function PostsList({ isPosting, onStopPosting }) {
         </Modal>
       )}
 
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>No posts yet!</h2>
           <p>Start adding some posts to see them here.</p>
         </div>
       )}
 
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} content={post.body} />
           ))}
         </ul>
+      )}
+
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>Loading posts...</h2>
+        </div>
       )}
     </>
   );
